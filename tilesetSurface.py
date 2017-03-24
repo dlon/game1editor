995844d3edbde32d3de7cde2ecb0b3307f1856c8
David L <dv.lnh.d@gmail.com>
2017-03-24 03:16:24 +0100

    tileset: select area

--- tilesetSurface.py
+++ tilesetSurface.py
@@ -1,11 +1,12 @@
 from PyQt5.QtWidgets import QFrame
 from PyQt5.QtGui import QColor, QPainter, QImage, QDrag, QPixmap
-from PyQt5.QtCore import Qt, QMimeData
+from PyQt5.QtCore import Qt, QMimeData, QRect
 
 class QTilesetSurface(QFrame):
 	def __init__(self, parent):
 		super().__init__(parent)
 		self.image = QImage()
+		self.selection = QRect()
 	def setImage(self, treeItem):
 		self.image.load("../data/%s" % treeItem.text(0))
 		self.repaint()
@@ -18,3 +19,17 @@ class QTilesetSurface(QFrame):
 			rect.top(),
 			self.image,
 		)
+		painter.setPen(QColor(255,0,0))
+		painter.drawRect(self.selection)
+	def mousePressEvent(self, e):
+		if self.image.isNull():
+			return
+		self.selection.setWidth(min(
+			16+16*int(e.x()/16),
+			self.image.width(),
+		))
+		self.selection.setHeight(min(
+			16+16*int(e.y()/16),
+			self.image.height(),
+		))
+		self.update()
