7413ee45d0e36ccc530082fbd7fb9e34425133c2
David L <dv.lnh.d@gmail.com>
2017-10-09 02:23:22 +0200

    resizeDialog: set size change

--- resizeDialog.py
+++ resizeDialog.py
@@ -4,12 +4,22 @@ from PyQt5 import QtGui, QtWidgets
 class ResizeDialog(QtWidgets.QDialog):
     def __init__(self, editorWindow):
         super().__init__(editorWindow)
-        resizeDlg = Ui_ResizeDialog()
-        resizeDlg.setupUi(self)
-        resizeDlg.widthEdit.setText(
+        self.editorWindow = editorWindow
+        self.resizeDlg = Ui_ResizeDialog()
+        self.resizeDlg.setupUi(self)
+        self.resizeDlg.widthEdit.setText(
             str(editorWindow.mapSurface.surfaceWidth)
         )
-        resizeDlg.heightEdit.setText(
+        self.resizeDlg.heightEdit.setText(
             str(editorWindow.mapSurface.surfaceHeight)
         )
-        resizeDlg.widthEdit.selectAll()
+        self.resizeDlg.widthEdit.selectAll()
+
+    def accept(self):
+        self.editorWindow.mapSurface.setWidth(
+            int(self.resizeDlg.widthEdit.text()), True
+        )
+        self.editorWindow.mapSurface.setHeight(
+            int(self.resizeDlg.heightEdit.text()), True
+        )
+        super().accept()
